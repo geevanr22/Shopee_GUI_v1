29bc17fdf8fa9b490f63273bd080045ba7b4eeb8
geevanr22 <geevanr2@gmail.com>
2022-04-17 11:39:03 +0800

    Search buttons opens search page

--- GUI_1/codeOpenChromeDebugger.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-
-# subprocess.call(['C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe'])
-# subprocess.call(['C:\\Users\\user\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\\Users\\User\\Desktop\\Projects\\Job Analyser\\webdriver_directory"'])
-
--- GUI_1/mainWindow.py
+++ GUI_1/mainWindow.py
@@ -1,27 +1,54 @@
+# Shopee Scraper with a GUI using PySide6
+
+from PySide6.QtWidgets import (QMainWindow, QDialog, QApplication, QPushButton, QHBoxLayout, QVBoxLayout)
 import sys
+from Product_Scraper import ProductScraperClass
 
-from PySide6.QtWidgets import (QMainWindow, QApplication, QPushButton, QHBoxLayout, QVBoxLayout)
 
 
-class MainWindow(QMainWindow):
+class MainWindow(QDialog):
     def __init__(self):
         super(). __init__()
         self.setWindowTitle('Shopee GUI')
         self.setGeometry(800, 100, 400, 200)
 
-        # self.openCommandPromptButton()
-        # self.layout()
+        # Initiate searchPushButton widget
+        self.searchButton = QPushButton('Search')
 
-    # def layout(self):
+        # connect searchButton to function that calls the openBrowser module and starts the browser and search functions
+        self.searchButton.clicked.connect(self.searchButtonFunctionCall)
+
+        self.layout()
+
+
+    def layout(self):
+        # initiate main layout
         self.mainLayout = QVBoxLayout()
-        self.cmdButtonLayout = QHBoxLayout()
-        self.cmdButton = QPushButton('Open Scraper')
-        # self.cmdButtonLayout.addWidget(self.cmdButton)
 
-        self.mainLayout.addWidget(self.cmdButton)
+        # Search Button Layout
+        self.searchButtonLayout = QHBoxLayout()
+        self.searchButtonLayout.addWidget(self.searchButton)
+
+        # add search button to main layout
+        self.mainLayout.addLayout(self.searchButtonLayout)
+
+        # set main layout to set layout
         self.setLayout(self.mainLayout)
 
-    # def openCommandPromptButton(self):
+
+    def searchButtonFunctionCall(self):
+        # calls the openBrowser module where the selenium scraper can take over
+        print('checking if search button works')
+        init_1 = ProductScraperClass('sex toys', 1)
+        init_1.open_search_page()
+
+
+
+
+
+
+
+
 
 
 
